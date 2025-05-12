8724620c39fb2fbdaf3d6d25134c83fd8db661ff
chris1797 <jaehun6004@gmail.com>
2025-05-12 23:57:55 +0900

    feat: board

---
+++ src/main/kotlin/core/carrotkotlin/domain/board/Board.kt
@@ -0,0 +1,34 @@
+package core.carrotkotlin.domain.board
+
+import core.carrotkotlin.domain.user.User
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+
+@Entity
+@EntityListeners(AuditingEntityListener::class)
+@Table(name = "board")
+class Board(
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    val user: User,
+
+    @Column(nullable = false)
+    val title: String,
+
+
+
+    ) {
+}
